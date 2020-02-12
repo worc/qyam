1097ee9ce47cf7735b4e5ae923e97f3dedb2ee4e
Curtis McAllister <cmcallister@workwithopal.com>
2020-02-11 18:22:50 -0800

    add sarcasm case converter

---
+++ src/sarcasm_case.js
@@ -0,0 +1,12 @@
+export default (text, startUpperCase = false) => {
+  let upper = startUpperCase
+
+  return text.split(' ').map(word => {
+    return word.split('').map(letter => {
+      upper = !upper
+      return upper
+        ? letter.toUpperCase()
+        : letter.toLowerCase()
+    }).join('')
+  }).join(' ')
+}
